248f6634d20db23b0813a7dc1a980da950c41f98
snkzt <snkzt@mercari.com>
2020-05-16 17:32:09 +0900

    enable an image pop up on click

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,9 @@
 document.getElementById("grid").addEventListener('click', yourMark);
 
 function yourMark(event) {
-  if (event.target.innerText === '') {
-     event.target.innerText = "O";
+  if (event.target.innerHTML === '') {
+     event.target.innerHTML = "<img src = 'https://raw.githubusercontent.com/snkzt/noughts-and-crosses/master/assets/image/X.png' height = '50px' width = '50px'>";
    } else {
-     event.target.innerText = '';
+     event.target.innerHTML = '';
      }
     }
