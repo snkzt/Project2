5638d3bf2d0eb07f617217250cf913e78fdd670c
snkzt <snkzt@mercari.com>
2020-05-16 22:49:16 +0900

    append/remove img on click

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,18 @@
 
 document.getElementById("grid").addEventListener('click', yourMark);
 
+var img = document.createElement("IMG");
+img.setAttribute("src", "https://raw.githubusercontent.com/snkzt/noughts-and-crosses/master/assets/image/X.png");
+img.setAttribute("height", "50px");
+img.setAttribute("width", "50px");
+img.setAttribute("alt", "X");
+
 function yourMark(event) {
-  if (event.target.innerHTML === '') {
-     event.target.innerHTML = "<img src = 'https://raw.githubusercontent.com/snkzt/noughts-and-crosses/master/assets/image/X.png' height = '50px' width = '50px'>";
+  if (event.target !== img) {
+     event.target.appendChild(img);
    } else {
-     event.target.innerHTML = '';
+     event.target.remove(img);
+     event.target.innerText = '';
      }
     }
+
