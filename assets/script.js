ebd455b9c418923c9c003427e8590c38f64c5399
snkzt <snkzt@mercari.com>
2020-05-31 20:22:03 +0900

    game logic fixed

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,69 @@
+let setCount = 0;
 
-document.getElementById("grid").addEventListener('click', yourMark);
+const winPattern = [
+  [0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+] 
 
-var img = document.createElement("IMG");
-img.setAttribute("src","image/X.png");
+document.getElementById("grid").addEventListener('click', turn);
 
-function yourMark(event) {
-  if (event.target !== img) {
-     event.target.appendChild(img);
-   } else {
-     event.target.remove(img);
-     event.target.innerText = '';
-     }
+function turn(event) {
+  if (event.target.childNodes.length === 0) {
+    setCount++;
+    if (setCount % 2 === 1) {
+      const x = document.createElement("IMG");
+      x.setAttribute("class","x");
+      x.setAttribute("src","image/X.png");
+      event.target.appendChild(x);
+    } else {
+      const o = document.createElement("IMG");
+      o.setAttribute("class","o");
+      o.setAttribute("src","image/O.png");
+      event.target.appendChild(o);
     }
+    checkWin();
+    checkTie();
+  } 
+}
+
+function checkWin() {
+  if (setCount >= 5) {
+    const cells = Array.from(document.querySelectorAll(".cell"));
+    const xArr = [];
+    const oArr = [];
+  cells.forEach(function(element){ 
+    if (element.childNodes.length === 1) {
+      element.childNodes.forEach (function (eachNode) {
+        if (eachNode.className === "x") { 
+          xArr.push(element.id);
+        } else {
+          if (eachNode.className === "o") {
+            oArr.push(element.id);    
+          }
+        }
+      })
+    }
+  })
+  winPattern.forEach(function(pattern) {
+    if (pattern.toString() === xArr.toString()) {
+      window.alert("x win!");
+    }
+    if (pattern.toString() === oArr.toString()) {
+      window.alert("o win!");
+    }
+  })
+  }
+}
+
+function checkTie () {
+  if (setCount === 9) {
+    window.alert("Tie game!");
+  }
+}
 
